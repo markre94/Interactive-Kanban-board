3e09957a0cdc0e55a7064d0a6c800000675f15f7
Marcin <marcin94@MacBook-Pro-Marcin.local>
2020-06-29 18:45:13 +0200

    Fixed first ver of data base

--- app.py
+++ app.py
@@ -2,16 +2,57 @@ from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+from sqlalchemy import Enum
+
 app = Flask('__name__')
 app.config ["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    status = db.Column()
+class Task(db.Model) :
+    """Task class
+    Attributes:
+        id (int): Unique id, primary key, auto increment.
+        username (str): Foreign key referencing Users table.
+        task (str): Details of the task.
+        status (enum): Status of the task, with the following values
+            - 'to_do'
+            - 'doing'
+            - 'done'
+    """
 
-    def __repr__(self) :
-        return '<Task %r>' % self.id
+    id = db.Column(db.Integer, primary_key=True)
+    # username = db.Column(db.String, db.ForeignKey('users.username'))
+    content = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(Enum('to_do', 'doing', 'done'))
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST' :
+        task_content = request.form ['content']
+        if task_content == '' :
+            flash('The task must contain something.', 'error')
+            return redirect('/')
+        else :
+            new_task = Task(content=task_content, status='to_do')
+
+            try :
+                db.session.add(new_task)
+                db.session.commit()
+                return redirect('')
+            except :
+                return 'There was an issue adding your task'
+
+    else :
+        tasks = Task.query.order_by(Task.date_created).all()
+
+        return render_template('main.html', tasks=tasks)
+
+@app.route('/delete')
+def delete():
+    pass
+
+if __name__ == "__main__" :
+    app.run(debug=True)
